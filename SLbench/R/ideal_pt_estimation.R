f05318555d4ac42adba34317a161b9f513f22e06
Jonathan Paul Olmsted <jpolmsted@gmail.com>
2011-09-23 17:40:02 -0400

    add ideal point estimation approach

---
+++ SLbench/R/ideal_pt_estimation.R
@@ -0,0 +1,43 @@
+################################################################################
+### FILENAME: R/ideal_pt_estimation.R
+###
+### AUTHOR: J. P. Olmsted --- jpolmsted@NOSPAM.gmail.com
+###
+### DATE: Fri Sep 23 15:12:49 2011
+###
+### LICENSE: See R Package Details.
+###
+### DESCRIPTION: This R Script file provides the testIPE1() and
+### testIPE2() functions based on example code from the "wnominate"
+### and "pscl" packages. It implement two different approaches to
+### ideal point estimation.
+################################################################################
+
+testIPE1 <- function()
+  {
+    require(wnominate)
+    data(sen90, package="wnominate")
+					# Load the roll call data used
+					# in this example.
+    foo <- wnominate(sen90,
+		     polarity=c(2,5),
+		     trials=5,
+                     verbose = TRUE
+		     )
+					# Use the PooleRosenthal
+					# approach to IPE.
+  }
+
+testIPE2 <- function{
+  require(wnominate)
+  require(pscl)
+  data(sen90, package="wnominate")
+					# Load the roll call data used
+					# in this example.
+  foo <- ideal(object = sen90,
+               store.item = TRUE,
+               verbose = TRUE
+               )
+}
+
+################################################################################
